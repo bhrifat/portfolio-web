f6d70e655cedf4af17800da8f6389b60ac63bab1
Badrul Hasan Rifat <badrul.h.rifat>
2021-05-05 00:20:06 +0600

    final Commit

---
+++ src/portfolio.js
@@ -0,0 +1,595 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello.",
+  title2: "Badrul Hasan Rifat",
+  logo_name: "<bhrifat/>",
+  nickname: "bhrifat",
+  full_name: "Badrul Hasan Rifat",
+  subTitle:
+    "Full Stack Developer, Machine Learning Enthusiast. Always learning.",
+  resumeLink:
+    "https://drive.google.com/file/d/1dX5tQFosP57B_TqDUDfsGkFQd64r63tg/view?usp=sharing",
+  mail: "mailto:badrul.h.rifat@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/bhrifat",
+  linkedin: "https://www.linkedin.com/in/bhrifat",
+  gmail: "badrul.h.rifat@gmail.com",
+  gitlab: " ",
+  facebook: "https://facebook.com/bhrifat",
+  twitter: "https://twitter.com/bhrifat",
+  instagram: "https://www.instagram.com/bhrifat/",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building responsive website front end using ReactJS",
+        "⚡ Developing mobile applications using Flutter and native Android",
+        "⚡ Creating application backend in Node, Express & Flask",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "PHP",
+          fontAwesomeClassname: "simple-icons:php",
+          style: {
+            color: "#7377AD",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+        // {
+        //   skillName: "GraphQL",
+        //   fontAwesomeClassname: "simple-icons:graphql",
+        //   style: {
+        //     color: "#DE33A6",
+        //   },
+        // },
+        {
+          skillName: "Android",
+          fontAwesomeClassname: "simple-icons:android",
+          style: {
+            color: "#3DDC84",
+          },
+        },
+        {
+          skillName: "Java",
+          fontAwesomeClassname: "simple-icons:java",
+          style: {
+            color: "#f89820",
+          },
+        },
+        {
+          skillName: "Flutter",
+          fontAwesomeClassname: "simple-icons:flutter",
+          style: {
+            color: "#02569B",
+          },
+        },
+        {
+          skillName: "Dart",
+          fontAwesomeClassname: "simple-icons:dart",
+          style: {
+            color: "#29B0EE",
+          },
+        },
+        // {
+        //   skillName: "Go",
+        //   fontAwesomeClassname: "simple-icons:go",
+        //   style: {
+        //     color: "#00A7D0",
+        //   },
+        // },
+        {
+          skillName: "Visual Basic",
+          fontAwesomeClassname: "simple-icons:dot-net",
+          style: {
+            color: "#029FCE",
+          },
+        },
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+        // {
+        //   skillName: "jQuery",
+        //   fontAwesomeClassname: "simple-icons:jquery",
+        //   style: {
+        //     color: "#0865A6",
+        //   },
+        // },
+        {
+          skillName: "Wordpress",
+          fontAwesomeClassname: "simple-icons:wordpress",
+          style: {
+            color: "#207297",
+          },
+        },
+        {
+          skillName: "Apache",
+          fontAwesomeClassname: "simple-icons:apache",
+          style: {
+            color: "#CA1A22",
+          },
+        },
+        // {
+        //   skillName: "C#",
+        //   fontAwesomeClassname: "simple-icons:csharp",
+        //   style: {
+        //     backgroundColor: "transparent",
+        //     color: "#3776AB",
+        //   },
+        // },
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "Bootstrap",
+          fontAwesomeClassname: "simple-icons:bootstrap",
+          style: {
+            color: "#3DDC84",
+          },
+        },
+        // {
+        //   skillName: "C++",
+        //   fontAwesomeClassname: "simple-icons:cplusplus",
+        //   style: {
+        //     color: "#E94E32",
+        //   },
+        // },
+      ],
+    },
+    // {
+    //   title: "Data Science & AI",
+    //   fileName: "DataScienceImg",
+    //   skills: [
+    //     "⚡ Experience of working with Computer Vision and NLP projects",
+    //     "⚡ Certifications by deeplearning.ai and Stanford Online",
+    //     "⚡ Experience with 10+ Projects",
+    //   ],
+    //   softwareSkills: [
+    //     {
+    //       skillName: "Tensorflow",
+    //       fontAwesomeClassname: "logos-tensorflow",
+    //       style: {
+    //         backgroundColor: "transparent",
+    //       },
+    //     },
+    //     {
+    //       skillName: "Keras",
+    //       fontAwesomeClassname: "simple-icons:keras",
+    //       style: {
+    //         backgroundColor: "white",
+    //         color: "#D00000",
+    //       },
+    //     },
+    //     {
+    //       skillName: "PyTorch",
+    //       fontAwesomeClassname: "logos-pytorch",
+    //       style: {
+    //         backgroundColor: "transparent",
+    //       },
+    //     },
+    //     {
+    //       skillName: "Python",
+    //       fontAwesomeClassname: "ion-logo-python",
+    //       style: {
+    //         backgroundColor: "transparent",
+    //         color: "#3776AB",
+    //       },
+    //     },
+    //     {
+    //       skillName: "Pandas",
+    //       fontAwesomeClassname: "simple-icons:pandas",
+    //       style: {
+    //         backgroundColor: "transparent",
+    //         color: "#6E6E6E",
+    //       },
+    //     },
+    //   ],
+    // },
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Experience hosting and managing websites",
+        "⚡ Deploying deep learning models on cloud to use on mobile devices",
+        "⚡ Experience with Continuous Integration",
+      ],
+      softwareSkills: [
+        {
+          skillName: "GCP",
+          fontAwesomeClassname: "simple-icons:googlecloud",
+          style: {
+            color: "#4285F4",
+          },
+        },
+        {
+          skillName: "AWS",
+          fontAwesomeClassname: "simple-icons:amazonaws",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Heroku",
+          fontAwesomeClassname: "simple-icons:heroku",
+          style: {
+            color: "#6863A6",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+        {
+          skillName: "PostgreSQL",
+          fontAwesomeClassname: "simple-icons:postgresql",
+          style: {
+            color: "#336791",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+        {
+          skillName: "Docker",
+          fontAwesomeClassname: "simple-icons:docker",
+          style: {
+            color: "#1488C6",
+          },
+        },
+        {
+          skillName: "Github",
+          fontAwesomeClassname: "simple-icons:github",
+          style: {
+            color: "#1488C6",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Yangzhou University",
+      subtitle: "BSc in Software Engineering",
+      logo_path: "yzu.png",
+      alt_name: "YZU",
+      duration: "2017 - 2021",
+      descriptions: [
+        "⚡ I have studied core subjects like Data Structures, DBMS, Networking, Security, etc.",
+        "⚡ I have also completed various online courses for ML/DL, Web Development, Mobile App Development, etc.",
+        "⚡ I have implemented several projects based on what I've leart under my Computer Engineering course. ",
+      ],
+      website_link: "http://en.yzu.edu.cn/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Python 3 Course",
+      subtitle: "sololearn.com",
+      logo_path: "solo-learn.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1Dr0H7smaU65EBGhVIdxgu1rPVbMdPCmF/view?usp=sharing",
+      alt_name: "sololearn.com",
+      color_code: "#47A048",
+    },
+    {
+      title: "Frontend Fundamentals",
+      subtitle: "pirple.com",
+      logo_path: "pirple-logo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1wE5iib9kYFge_Ef21hhu-PWR5NaGym-2/view?usp=sharing",
+      alt_name: "pirple.com",
+      color_code: "#F6B808",
+    },
+    {
+      title: "Digital Security",
+      subtitle: "Digital Security Agenncy under Bangladesh Government",
+      logo_path: "Digital-Security-Agency.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1zPeGlfaC4cAV5vTBnfKd8qYGBhGHDk2Y/view?usp=sharing",
+      alt_name: "Digital Security Agenncy under Bangladesh Government",
+      color_code: "#2AAFED",
+    },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "I've completed two internships. I've mostly done projects on my own and I am actively looking for internships. I love organizing workshops to share my knowledge with others.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Internships",
+      experiences: [
+        {
+          title: "HR Assistant",
+          company: "Gao Tek",
+          company_url: "https://gaotek.com/",
+          logo_path: "gaotek.png",
+          duration: "April 2021 - Present",
+          location: "Dhaka, Bangladesh",
+          description:
+            "Assisting in the day-to-day tasks of HR roles and responsibilities. Supporting Human Resources administrators with clerical and managerial tasks. Compiling and reviewing staff information (hard and soft copies).",
+          color: "#ee3c26",
+        },
+      ],
+    },
+    // {
+    //   title: "Volunteerships",
+    //   experiences: [
+    //     {
+    //       title: "Google Summer of Code Mentor",
+    //       company: "Sugar Labs / GSoC",
+    //       company_url: "https://sugarlabs.org/",
+    //       logo_path: "google_logo.png",
+    //       duration: "Summer of 2017 / 18 / 20",
+    //       location: " ",
+    //       description:
+    //         "Mentorship responsibilities were to help students plan the project, review issues and pull requests, ensure smooth progress and help them out when they are stuck.",
+    //       color: "#4285F4",
+    //     },
+    //     {
+    //       title: "Google Code-In Mentor",
+    //       company: "Sugar Labs / GSoC",
+    //       company_url: "https://sugarlabs.org/",
+    //       logo_path: "google_logo.png",
+    //       duration: "Winter of 2017 / 18 / 19",
+    //       location: " ",
+    //       description:
+    //         "Day to day resposibilites of this mentorship was to help out children aged 13-17 get started with programming and open-source, review their work and approve them.",
+    //       color: "#D83B01",
+    //     },
+    //     {
+    //       title: "Board Member at Codeuino",
+    //       company: " ",
+    //       company_url: " ",
+    //       logo_path: "codeuino.jpg",
+    //       duration: "2018 - 2019",
+    //       location: " ",
+    //       description: "Previous Board during early days of Codeuino",
+    //       color: "#D83B01",
+    //     },
+    //     {
+    //       title: "BluWings Club",
+    //       company: " ",
+    //       company_url: " ",
+    //       logo_path: "b.png",
+    //       duration: "2018 - On Going",
+    //       location: " ",
+    //       description:
+    //         "Co-Founder and President of the programming club called BluWings. Have organized several workshops which introduced programming and related tools to freshers.",
+    //       color: "#D83B01",
+    //     },
+    //   ],
+    // },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects makes use of vast variety of latest technology tools. My best experience is to create Data Science projects. Below are some of my projects. Note that not all of the mentioned projects are on GitHub yet.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "bhrifat.png",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Medium.",
+    link: "https://medium.com/@badrul-rifat",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      id: "0",
+      name: "Portfolio",
+      url: "https://github.com/bhrifat/portfolio-web.git",
+      description: "(This) A clean and full customizable portfolio template.",
+      languages: [
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+    {
+      id: "1",
+      name: "eShop",
+      url: "https://eshop-6d048.web.app/ ",
+      description:
+        "Driver drowsiness detection, profanity detection, violence detection, SMS alerts, cab driver tracking.",
+      languages: [
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "NodeJS",
+          iconifyClass: "logos:nodejs-icon",
+        },
+        {
+          name: "MongoDB",
+          iconifyClass: "vscode-icons:file-type-mongo",
+        },
+      ],
+    },
+    {
+      id: "2",
+      name: "Hungry Monster",
+      url: " https://bhrifat.github.io/hungry-monster/",
+      description:
+        "Published on Play Store back in 2017, crossed 55K downloads. Performed object detection, image captioning, OCR, handwritten OCR, barcode and QR code scanning with product information.",
+      languages: [
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Firebase",
+          iconifyClass: "logos-firebase",
+        },
+      ],
+    },
+    {
+      id: "12",
+      name: "Football Mania",
+      url: "https://football-mania24.netlify.app/",
+      description:
+        "A sound-classifier webapp made with ReactJS + TensorflowJS.",
+      languages: [
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Firebase",
+          iconifyClass: "logos-firebase",
+        },
+      ],
+    },
+    {
+      id: "13",
+      name: "Quotes Generator",
+      url: "https://bhrifat.github.io/quetos-generator/",
+      description:
+        "A video summarization webapp. A Flask-based backend which servers the Tensorflow model and a frontend made in ReactJS.",
+      languages: [
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+      ],
+    },
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
